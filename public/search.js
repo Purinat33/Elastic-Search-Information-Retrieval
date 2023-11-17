51275babef0b4d1dffad03b1223d8485f4b4e741
Purinat33 <Purinat33@gmail.com>
2023-11-18 00:29:30 +0700

    Styled

--- public/search.js
+++ public/search.js
@@ -34,21 +34,57 @@ function searchElasticsearch(query) {
 //     });
 // }
 
+// function displaySearchResults(results) {
+//     const searchResults = document.getElementById('searchResults');
+//     searchResults.innerHTML = '';
+
+//     results.forEach(result => {
+//         const resultElement = document.createElement('div');
+//         resultElement.classList.add('result-box'); // Add the 'result-box' class
+
+//         resultElement.innerHTML = `<h3>${result._source.Name}</h3><p>${result._source.Description}</p>`;
+        
+//         // Check if the Website property exists before appending it to the resultElement
+//         if (result._source.Website) {
+//             resultElement.innerHTML += `<a href="${result._source.Website}" target="_blank">Visit Website</a>`;
+//         }
+
+//         searchResults.appendChild(resultElement);
+//     });
+// }
+
+// Check if the search input is empty when the page loads
+window.addEventListener('load', function() {
+    const searchTerm = document.getElementById('searchInput').value;
+    if (searchTerm.trim() === '') {
+        searchElasticsearch(searchTerm);
+    }
+});
+
+
+// Function to display search results or a "No Results Found" card
 function displaySearchResults(results) {
     const searchResults = document.getElementById('searchResults');
     searchResults.innerHTML = '';
 
-    results.forEach(result => {
-        const resultElement = document.createElement('div');
-        resultElement.classList.add('result-box'); // Add the 'result-box' class
+    if (results.length === 0) {
+        const noResultsCard = document.createElement('div');
+        noResultsCard.classList.add('result-box');
 
-        resultElement.innerHTML = `<h3>${result._source.Name}</h3><p>${result._source.Description}</p>`;
-        
-        // Check if the Website property exists before appending it to the resultElement
-        if (result._source.Website) {
-            resultElement.innerHTML += `<a href="${result._source.Website}" target="_blank">Visit Website</a>`;
-        }
+        noResultsCard.innerHTML = `<p>No Results Found</p>`;
+        searchResults.appendChild(noResultsCard);
+    } else {
+        results.forEach(result => {
+            const resultElement = document.createElement('div');
+            resultElement.classList.add('result-box');
 
-        searchResults.appendChild(resultElement);
-    });
+            resultElement.innerHTML = `<h3>${result._source.Name}</h3><p>${result._source.Description}</p>`;
+
+            if (result._source.Website) {
+                resultElement.innerHTML += `<a href="${result._source.Website}" target="_blank">Visit Website</a>`;
+            }
+
+            searchResults.appendChild(resultElement);
+        });
+    }
 }
